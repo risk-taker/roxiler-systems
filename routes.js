02d90679d3e8a74e5f3ce3337cbf034df03d8ebd
risk-taker <deepuku5698@gmail.com>
2022-06-10 23:59:48 +0530

    change routes

--- routes.js
+++ routes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const controller = require('./controller');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.json({ status: 1 });
-})
-
 router.get('/todos', controller.todos);
 router.get('/user/:id', controller.user);
 module.exports = router;
